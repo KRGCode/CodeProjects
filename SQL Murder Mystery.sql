d1c2336a64c1b0b5de6ad731656995be1b666819
Ryan <76857512+KRGCode@users.noreply.github.com>
2022-03-02 10:49:02 -0800

    Create SQL Murder Mystery.sql

---
+++ SQL Murder Mystery.sql
@@ -0,0 +1,10 @@
+#Current Code for Solving the SQL Murder Mystery at https://mystery.knightlab.com/
+
+Select *
+from crime_scene_report
+where type = 'murder'
+and city = 'SQL City'
+
+
+#20180115	murder	Security footage shows that there were 2 witnesses. The first witness lives at the last house on "Northwestern Dr". 
+The second witness, named Annabel, lives somewhere on "Franklin Ave".	SQL City
