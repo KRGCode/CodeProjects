ad9e1b49d180c4dc46f82a84410b7df36bc5075c
Ryan <76857512+KRGCode@users.noreply.github.com>
2022-04-17 11:25:15 -0700

    Update SonicPi Haunted Space Sounds.rb

--- SonicPi Haunted Space Sounds.rb
+++ SonicPi Haunted Space Sounds.rb
@@ -25,3 +25,29 @@ with_fx :flanger, mix: 0.7 do
   end
 end
 
+
+#Continious Spook Mix ****************************************8
+
+use_synth :rodeo
+with_fx :flanger, mix: 0.7 do
+  with_fx :echo do
+    
+    live_loop :noteseq do
+      sample :elec_ping
+      play choose([:D4,:F4, :Ds4]), attack: 2, release: 6
+      sleep rrand(3,5)
+    end
+    
+    live_loop :noteseq do
+      sample :drum_cymbal_open
+      play choose([:Fs4,:G4, :E3]), attack: 2, release: 5
+      sleep rrand(3,4)
+    end
+    
+    live_loop :noteseq do
+      sample :ambi_glass_hum
+      play choose([:A4, :C5, :A2]), attack: 2, release: 5
+      sleep rrand(3,6)
+    end
+  end
+end
