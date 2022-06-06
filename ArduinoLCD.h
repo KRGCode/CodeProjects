49b80441a14f9ccf4e55f757576942907357a494
Ryan <76857512+KRGCode@users.noreply.github.com>
2022-06-05 17:33:37 -0700

    Create ArduinoLCD.h

---
+++ ArduinoLCD.h
@@ -0,0 +1,32 @@
+#Spark Electronics--https://www.sparkfun.com/products/10097
+
+#include <LiquidCrystal.h>          
+LiquidCrystal lcd(13, 12, 11, 10, 9, 8);    
+void setup() {                     
+  lcd.begin(16, 2);                  
+
+}
+
+void loop() {
+  lcd.setCursor(4,0);               
+  lcd.print ("KRGCode");         
+  lcd.setCursor(3,2);
+  lcd.print ("On Github");
+  lcd.setCursor(0,0);
+  lcd.print ("*");
+  lcd.setCursor(0,2);
+  lcd.print ("*");
+  lcd.setCursor(15,0);
+  lcd.print ("*");
+  lcd.setCursor(15,2);
+  lcd.print ("*");
+  
+
+// Turn on the blinking cursor:
+  lcd.blink();
+  delay(1000);
+   
+
+lcd.setCursor(0,2);
+lcd.print ("");
+}  
