28e74072648ca88743573e4ec56b413bf1d83648
Ryan <76857512+KRGCode@users.noreply.github.com>
2022-08-05 20:31:04 -0700

    Update and rename PixelLCDArtProject.h to MovingGhostPixelLCDArtProject.h

--- MovingGhostPixelLCDArtProject.h
+++ MovingGhostPixelLCDArtProject.h
@@ -1,20 +1,26 @@
-#include <LiquidCrystal.h>
-
+#include <LiquidCrystal.h>       
 LiquidCrystal lcd(13,12, 11, 10, 9, 8);
 
-byte smiley[8] = {
+byte smiley[13] = {
   B11110,
   B01111,
+  B01010,
+  B11111,
+  B11111,
+  B11001,
+  B11111,
+  B00000,
   B11111,
   B01111,
   B11011,
-  B01111,
+  B00011,
   B11110,
 };
 
 void setup () {
   lcd.createChar(0, smiley);
   lcd.begin(16, 2);  
+  lcd.setCursor(0, 2);
   lcd.setCursor(7, 2);
   lcd.write(byte(0));
   lcd.setCursor(8, 0);
@@ -31,7 +37,7 @@ void setup () {
   lcd.write(byte(0));
   lcd.setCursor(15, 1);
   lcd.write(byte(0));
-  
+
 }
 
 
