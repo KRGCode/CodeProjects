f8df60ac76b81e012535817e300ce395004423ed
Ryan <76857512+KRGCode@users.noreply.github.com>
2024-06-09 18:32:49 -0700

    Delete HauntingStart.rb

--- HauntingStart.rb
+++
@@ -1,9 +0,0 @@
-in_thread do
-  live_loop :noteseq do
-    sample :bass_thick_c
-    sample :ambi_glass_rub, release: 1
-    sample :elec_chime
-    play choose([:D4,:F4, :Ds4, :E3, :F4]), attack: 2, release: 6
-    sleep rrand(3,5)
-  end
-end
