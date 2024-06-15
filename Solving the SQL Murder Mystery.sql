3f962f37d02a015ecc5c661cfc4445b181d47c8f
Ryan <76857512+KRGCode@users.noreply.github.com>
2024-06-15 13:11:47 -0700

    Update Solving the SQL Murder Mystery.sql

--- Solving the SQL Murder Mystery.sql
+++ Solving the SQL Murder Mystery.sql
@@ -1,5 +1,5 @@
 /*The SQL Murder Mystery-https://mystery.knightlab.com//
-/*You vaguely remember that the crime was a ​murder​ that occurred sometime on ​Jan.15, 2018​ and that it took place in ​SQL City​.
+/*You vaguely remember that the crime was a ​murder​ that occurred sometime on ​Jan.15, 2018​ and that it took place in ​SQL City​.*/
 
 Select * from crime_scene_report
 where city is "SQL City" 
@@ -10,5 +10,5 @@ and date is "20180115";
 Date: 20180115, Type: Murder, City: SQL City
 Description: Security footage shows that there were 2 witnesses. 
 The first witness lives at the last house on "Northwestern Dr". 
-The second witness, named Annabel, lives somewhere on "Franklin Ave"./
+The second witness, named Annabel, lives somewhere on "Franklin Ave".*/
 
