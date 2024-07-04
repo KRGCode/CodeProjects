18a6e1c324263770a33fbb00f6c444ca38de1960
Ryan <76857512+KRGCode@users.noreply.github.com>
2024-07-04 14:19:55 -0700

    Update PostgreSqlFTCDatea.sql

--- PostgreSqlFTCDatea.sql
+++ PostgreSqlFTCDatea.sql
@@ -1,3 +1,8 @@
+/*Fraud is at an all time high, this data review shows the highest fraud categories reported to the Federal Trade Commission (FTC) 
+with data obtained from the FTC Consumer Sentinel Network Data Book-https://www.ftc.gov/policy-notices/open-government/data-sets */
+
+/* I used ProstgreSQl to create a table and visualizations using the 2023_CSN_Report_Categories */
+
 Create Table CSN_Report_Categories (
 	CSN_Rank varchar (90),
 	CSN_Category varchar (90),
@@ -11,4 +16,4 @@ Copy csn_report_categories
 from 'C:\Users\Private\Private\Desktop\2023_CSN_Report_Categories.csv'
 with (Format CSV, Header);
 
-*/Pleae note that Line/*
+/*Pleae note that Lines 34 and 36 were removed as it was additonal details provided by the FTC*\
