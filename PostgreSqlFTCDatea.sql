3fda5aff3ab80395c9c44aba1259f4f386f80bbe
Ryan <76857512+KRGCode@users.noreply.github.com>
2024-07-04 13:09:56 -0700

    Create PostgreSqlFTCDatea.sql

---
+++ PostgreSqlFTCDatea.sql
@@ -0,0 +1,14 @@
+Create Table CSN_Report_Categories (
+	CSN_Rank varchar (90),
+	CSN_Category varchar (90),
+	CSN_Number_Of_Report varchar (90),
+	CSN_Percentage varchar (90)
+);
+
+_____________________________________________
+
+Copy csn_report_categories
+from 'C:\Users\Private\Private\Desktop\2023_CSN_Report_Categories.csv'
+with (Format CSV, Header);
+
+*/Pleae note that Line/*
