5698d90548dfa2fb4ee4e9472cb91160dd9a9e4e
Ryan <76857512+KRGCode@users.noreply.github.com>
2024-10-05 14:46:04 -0700

    Create FTCIdentityTheftbyTheft.sql

---
+++ FTCIdentityTheftbyTheft.sql
@@ -0,0 +1,2 @@
+/* Exploration of the 2023 Identity Theft Reports by state obtained from https://www.ftc.gov/policy-notices/open-government/data-sets, lines 1, 369, and 371 were removed
+to upload report to pgAdmin 4 for analysis. Percentage sign, commas, and hashtag removed for upload. 
